4993774c069cec8b31fd248b6a9c30d9f8150762
rpgoldman <rpgoldman@sift.net>
2023-04-22 11:28:41 -0500

    Update conf.py
    Fix path so that sphinx can find pydcop.

--- docs/conf.py
+++ docs/conf.py
@@ -18,8 +18,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- General configuration ------------------------------------------------
@@ -80,7 +80,7 @@ release = __version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
